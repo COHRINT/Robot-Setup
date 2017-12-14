296328a04fc729fc79ba8b6398a32ae31d08ae75
Luke Barbier <lukebarbier12@gmail.com>
2017-12-14 12:05:14 -0700

    dynamic hostname setup

--- install/cnr_setup.sh
+++ install/cnr_setup.sh
@@ -16,7 +16,7 @@ fi
 sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 
-echo 'Plug in the ethernet cable and press [ENTER]'
+echo 'Plug in the ethernet cable and press [ENTER] (Install speed is much faster)'
 read n -1
 
 apt-get update
@@ -39,7 +39,6 @@ cp turlebot_files/turtlebot_node.py /opt/ros/kinetic/lib/create_node/
 # Add Turtlebot Variables
 echo 'source /opt/ros/kinetic/setup.bash' >> /home/odroid/.bashrc
 echo 'export ROS_MASTER_URI=http://${SSH_CLIENT%% *}:11311' >> /home/odroid/.bashrc
-echo 'export ROS_HOSTNAME=192.168.20.124' >> /home/odroid/.bashrc
 echo 'export ROBOT=$HOSTNAME' >> /home/odroid/.bashrc
 
 echo 'export TURTLEBOT_3D_SENSOR=kinect' >> /home/odroid/.bashrc
@@ -47,10 +46,31 @@ echo 'export TURTLEBOT_STACKS=circles' >> /home/odroid/.bashrc
 echo 'export TURTLEBOT_BASE=create' >> /home/odroid/.bashrc
 echo 'export TURTLEBOT_SERIAL_PORT=/dev/ttyUSB0' >> /home/odroid/.bashrc
 
+# Find the hostname
+case "$HOSTNAME" in
+    "deckard")
+	export rob_ip="121"
+	;;
+    "roy")
+	export rob_ip="122"
+	;;
+    "pris")
+	export rob_ip="123"
+	;;
+    "zhora")
+	export rob_ip="124"
+	;;
+    *) echo "Robot hostname not properly setup"
+       echo "change the bashrc to properly reflect the ROS_HOSTNAME number"
+       export rob_ip="#"
+       ;;
+
+echo 'export ROS_HOSTNAME=192.168.20.'$rob_ip >> /home/odroid/.bashrc
+
 source /home/odroid/.bashrc
 
 echo "---------------------------"
-echo 'Press [ENTER] to reboot. This robot is now a fully functional cop and robber.'
+echo 'Press [ENTER] to reboot. '$HOSTNAME' is now a fully functional cop and robber.'
 read -n 1
 
 reboot
